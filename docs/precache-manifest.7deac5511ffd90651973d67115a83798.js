2818238af2e3faa34fe2de56664bb5ca52e7313f
Arpit bansal <arpit.bansal2010@gmail.com>
2018-11-23 01:51:24 +0530

    docs folder for github pages

---
+++ docs/precache-manifest.7deac5511ffd90651973d67115a83798.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ab18e5a06febd1b5077",
+    "url": "/static/js/main.8ab18e5a.chunk.js"
+  },
+  {
+    "revision": "504f132680068c63f8bc",
+    "url": "/static/js/1.504f1326.chunk.js"
+  },
+  {
+    "revision": "8ab18e5a06febd1b5077",
+    "url": "/static/css/main.6a8b1591.chunk.css"
+  },
+  {
+    "revision": "77e3d231d85209efeb7fffb2ee8351e1",
+    "url": "/index.html"
+  }
+];
