161095714a4f87a0db2461d66f7fa7a0a5666234
Arpit bansal <arpit.bansal2010@gmail.com>
2018-11-23 02:06:37 +0530

    docs folder

---
+++ docs/precache-manifest.1d96129981fb89f6191ae82b5810ad4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5176f7661845551bc18",
+    "url": "/superfund/static/js/runtime~main.b5176f76.js"
+  },
+  {
+    "revision": "386273040dc752ea31c2",
+    "url": "/superfund/static/js/main.38627304.chunk.js"
+  },
+  {
+    "revision": "504f132680068c63f8bc",
+    "url": "/superfund/static/js/1.504f1326.chunk.js"
+  },
+  {
+    "revision": "386273040dc752ea31c2",
+    "url": "/superfund/static/css/main.6a8b1591.chunk.css"
+  },
+  {
+    "revision": "d9d1b971b7eb20fdabfc6d29d8534f50",
+    "url": "/superfund/index.html"
+  }
+];
